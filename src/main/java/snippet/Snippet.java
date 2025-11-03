c7de5e150e1472045a0f95aa650844322e7a0cad
k-s-jutjoy <kos.saito.jutjoy@gmail.com>
2025-11-03 16:29:45 +0900

    確認

---
+++ src/main/java/snippet/Snippet.java
@@ -0,0 +1,17 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		 javax.validation.GroupSequence;
+		 javax.validation.constraints.Email;
+		 javax.validation.constraints.Max;
+		 javax.validation.constraints.Min;
+		 javax.validation.constraints.NotBlank;
+		 javax.validation.constraints.NotNull;
+		 javax.validation.constraints.Size;
+		
+		import lombok.Data;
+		
+	}
+}
+
