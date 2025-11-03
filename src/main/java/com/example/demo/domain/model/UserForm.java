cee428de88e32ad14a1cbb4979297de05ce82933
k-s-jutjoy <kos.saito.jutjoy@gmail.com>
2025-11-03 17:05:54 +0900

    修正

--- src/main/java/com/example/demo/domain/model/UserForm.java
+++ src/main/java/com/example/demo/domain/model/UserForm.java
@@ -1,14 +1,13 @@
 package com.example.demo.domain.model;
 
-import javax.validation.GroupSequence;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-
+import jakarta.validation.GroupSequence;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
--- src/main/java/snippet/Snippet.java
+++ src/main/java/snippet/Snippet.java
@@ -1,17 +1,38 @@
 package snippet;
 
-public class Snippet {
-	public static void main(String[] args) {
-		 javax.validation.GroupSequence;
-		 javax.validation.constraints.Email;
-		 javax.validation.constraints.Max;
-		 javax.validation.constraints.Min;
-		 javax.validation.constraints.NotBlank;
-		 javax.validation.constraints.NotNull;
-		 javax.validation.constraints.Size;
-		
-		import lombok.Data;
-		
-	}
-}
 
+
+import jakarta.validation.GroupSequence;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Snippet {
+
+    // 必須入力、20文字以下であること
+    @NotBlank(message = "名前を入力してください。", groups = Group1.class)
+    @Size(min = 1, max = 20, message = "名前を20文字以内で入力してください。", groups = Group2.class)
+    private String name;
+
+    // Email形式であること
+    @Email(message = "E-Mailを正しい形式で入力してください。", groups = Group1.class)
+    private String email;
+
+    // 必須入力、入力値が0～100であること
+    @NotNull(message = "年齢を入力してください。", groups = Group1.class)
+    @Min(value = 0, message = "年齢は0以上を入力してください。", groups = Group2.class)
+    @Max(value = 100, message = "年齢は100以下を入力してください。", groups = Group2.class)
+    private Integer age;
+
+    public interface Group1 {}
+    public interface Group2 {}
+
+    @GroupSequence({Group1.class, Group2.class})
+    public interface Groups {}
+
+}
